2c88d74a3c8b2adf2ecf6048cd0b0ca13f1acd03
cristian2306 <cristian.castellanos@mail.escuelaing.edu.co>
2022-10-24 16:51:44 -0500

    Commit prueba

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import OperativePlan from './components/OperativePlan';
 
 
 function App() {
   return (
     <div className="App">
           <Header />
+          <OperativePlan />
     </div>
   );
 }
