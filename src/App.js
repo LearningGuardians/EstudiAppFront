c8faf1d632cd48c4ba4323fe769203ee12d78ff8
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2022-10-25 10:56:31 -0500

    Primera pagina plan operativo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Grid from '@mui/material/Grid';
 import OperativePlan from './components/OperativePlan';
+import { List } from '@mui/material';
+import FirstOperative from './Pages/FirstOperative';
 
 
 function App() {
   return (
     <div className="App">
-          <Header />
-          <OperativePlan />
+          <FirstOperative />
     </div>
   );
 }
--- src/components/OperativePlan.js
+++ src/components/OperativePlan.js
@@ -1,35 +1,19 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import AspectRatio from '@mui/joy/AspectRatio';
+import Card from '@mui/joy/Card';
+import Typography from '@mui/joy/Typography';
+import ImageIcon from '@mui/icons-material/Image';
 
-export default function MultiActiOperativePlan(){
+export default function BasicRatio() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
+    <Card variant="outlined" sx={{ width: 300 }}>
+      <AspectRatio>
+        <div>
+          <ImageIcon fontSize="xl5" sx={{ color: 'text.tertiary' }} />
+        </div>
+      </AspectRatio>
+      <Typography mt={2}>Title</Typography>
+      <Typography level="body2">Description of the card.</Typography>
     </Card>
   );
 }
