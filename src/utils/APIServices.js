3ee61d4b7afd37d9083bf14e27ee54a8fc0a86a0
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2022-10-27 14:44:50 -0500

    Arreglo minimo

--- src/utils/APIServices.js
+++ src/utils/APIServices.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 let UseFetchGET=  () => {
-    var URL_API = 'http://localhost:8080/pOperativo/all/Jose';
+    var URL_API = 'http://localhost:8080/operativo/all/Jose';
     axios.get(URL_API)
         .then(function(res){
-        console.log("FUNCIONAL")
+        console.log(res.data)
         })
         .catch(function (error) {
             console.log(error)
