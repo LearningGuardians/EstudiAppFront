d44ea28d6cca4b25079c6b692d2df016cce1eead
unknown <cristian.forero-m@labinfo.is.escuelaing.edu.co>
2022-10-27 16:05:13 -0500

    Avance Landing Page

--- src/Pages/Subjects.js
+++ src/Pages/Subjects.js
@@ -2,3 +2,10 @@ import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from '../components/Header';
 
+ function Subjects(props){
+     return (
+        <Header/>
+     );
+ }
+
+ export default Subjects
--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import FirstOperative from "../Pages/FirstOperative";
+import LandingPage from "../Pages/LandingPage";
 
 
 export default function AppRoutes() {
@@ -15,6 +16,7 @@ export default function AppRoutes() {
                 <div className='container'>
                     <Routes>
                         <Route path="/pOperativo" element={<FirstOperative />} />
+                        <Route path="/landing" element ={<LandingPage/>}/>
                     </Routes>
                 </div>
             </main>
